3d87cea52815c37bcbd18032369155bb1f3177f6
Dominique Christopher <showlandproductions@gmail.com>
2021-01-19 17:47:18 -0800

    Add files via upload
    Added Packages and NPM's to index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+
+//NPM's used
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
